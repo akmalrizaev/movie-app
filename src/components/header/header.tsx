ce4275f99aff9b56da9bc27a06b4dc9493c5bcd7
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-10-02 18:58:20 +0500

    creating Navbar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,25 @@
 import Image from 'next/image';
 import { VscAccount } from 'react-icons/vsc';
-import { AiOutlineSearch } from 'react-icons/ai';
+import { AiOutlineSearch, AiOutlineUser } from 'react-icons/ai';
+import { BiBellMinus } from 'react-icons/bi';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
-    <header>
+    <header className={`${scrolled && 'bg-[#E10856]'}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <Image
           src={'/logo.svg'}
@@ -22,8 +37,12 @@ const Header = () => {
         </ul>
       </div>
       <div className="flex item-center space-x-4 text-sm font-light">
-        <AiOutlineSearch />
-        <VscAccount />
+        <AiOutlineSearch className="h-6 w-6 cursor-pointer" />
+        <p className="hidden lg:inline">Kids</p>
+        <BiBellMinus className="h-6 w-6 cursor-pointer" />
+        <Link href={'/account'}>
+          <AiOutlineUser className="h-6 w-6 cursor-pointer" />
+        </Link>
       </div>
     </header>
   );
