1e1e06caed1aa8f5d5bc4832d374eaddd203c952
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-10-04 16:24:53 +0500

    Review average

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -4,6 +4,7 @@ import { image_base } from 'src/helpers/constants';
 import { IMovie } from 'src/interfaces/app.interface';
 import { HeroProps } from './hero.props';
 import { TbPlayerPlay } from 'react-icons/tb';
+import ReactStars from 'react-stars';
 
 const Hero = ({ trending }: HeroProps): JSX.Element => {
   const [movie, setMovie] = useState<IMovie>({} as IMovie);
@@ -13,7 +14,7 @@ const Hero = ({ trending }: HeroProps): JSX.Element => {
     setMovie(randomMovie);
   }, [trending]);
   return (
-    <div className="flex flex-col space-y-2 py-20 md:space-y-4 lg:h-[65vh] lg:pb-12 lg:justify-end">
+    <div className="flex flex-col space-y-2 py-20 md:space-y-4 lg:h-[65vh] lg:pb-12 lg:center">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-full">
         <Image
           src={`${image_base}${movie?.backdrop_path || movie?.poster_path}`}
@@ -23,6 +24,20 @@ const Hero = ({ trending }: HeroProps): JSX.Element => {
         />
       </div>
 
+      <div className="py-[4px] px-[8px] text-center rounded-bl-[8px] rounded-tr-[4px] w-[111px] bg-[#1d1d1d]/50 inline-block">
+        {movie.media_type}
+      </div>
+
+      <div className="flex items-center space-x-2">
+        <ReactStars
+          edit={false}
+          count={10}
+          value={movie.vote_average}
+          color2={'#fff'}
+        />
+        <p>({movie.vote_count})</p>
+      </div>
+
       <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
@@ -30,7 +45,7 @@ const Hero = ({ trending }: HeroProps): JSX.Element => {
         {movie?.overview}
       </p>
       <div>
-        <button className="flex justify-center items-center space-x-2 bg-white/40 font-bold text-black w-[200px] h-[56px] rounded-full">
+        <button className="flex justify-center items-center space-x-2 bg-white/60 font-bold text-black w-[200px] h-[56px] rounded-full">
           <TbPlayerPlay className="h-5 w-5 md:h-8 md:w-8" /> Watch now
         </button>
       </div>
