71a96b45d7c04ab35184d10456e152ba2477bb49
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-10-09 11:37:05 +0500

    firebase Sign In, Sign Out form

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,7 +1,13 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import { useState } from 'react';
 import AuthComponent from 'src/components/AuthComponent';
 
 const Auth = () => {
+  const [auth, setAuth] = useState<'signup' | 'signin'>('signin');
+  const toggleAuth = (state: 'signup' | 'signin') => {
+    setAuth(state);
+  };
   return (
     <>
       <Head>
@@ -13,6 +19,64 @@ const Auth = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Image
+        src={'/logo.svg'}
+        alt={'logo'}
+        width={70}
+        height={70}
+        className={' absolute left-4 top-4 cursor-pointer object-contain'}
+      />
+      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:mx-14">
+        <h1 className="text-4xl font-semibold">
+          {auth === 'signup' ? 'Sign Up' : 'Sign In'}
+        </h1>
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input type="text" placeholder="Email" className="input" />
+          </label>
+          <label className="inline-block w-full">
+            <input type="password" placeholder="Password" className="input" />
+          </label>
+        </div>
+        {auth === 'signin' ? (
+          <button
+            type="submit"
+            className="w-full bg-[#E10856] py-3 font-semibold"
+          >
+            Sign In
+          </button>
+        ) : (
+          <button
+            type="submit"
+            className="w-full bg-[#E10856] py-3 font-semibold"
+          >
+            Sign Up
+          </button>
+        )}
+        {auth === 'signin' ? (
+          <div className="text-[gray]">
+            Not yet account?
+            <button
+              type="button"
+              className="text-white hover:underline"
+              onClick={() => toggleAuth('signup')}
+            >
+              Sign Up Now
+            </button>
+          </div>
+        ) : (
+          <div className="text-[gray]">
+            Already have account?
+            <button
+              type="button"
+              className="text-white hover:underline"
+              onClick={() => toggleAuth('signin')}
+            >
+              Sign In
+            </button>
+          </div>
+        )}
+      </form>
       <AuthComponent />
     </>
   );
